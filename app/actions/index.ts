8bf1172b4fb54d7f2a41e2d8d50b6e48a145f15e
@zacbe <zacbegonzalez@gmail.com>
2024-06-04 22:59:49 +0200

    chore:  add entry point for actions

---
+++ app/actions/index.ts
@@ -0,0 +1,23 @@
+import {
+  createOneLesson,
+  findOneLesson, 
+  deleteOneLesson,
+  findAllLessons
+} from "./lessons";
+import {
+  createOneChallenge,
+  findOneChallenge,
+  deleteOneChallenge,
+  findAllChallenges
+} from "./challenges";
+
+export {
+  createOneLesson,
+  findOneLesson,
+  deleteOneLesson,
+  findAllLessons,
+  createOneChallenge,
+  findOneChallenge,
+  deleteOneChallenge,
+  findAllChallenges
+};
