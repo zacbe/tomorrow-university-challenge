ff59c3b1fa8d4c74b9e75e8c9498623089e5e651
@zacbe <zacbegonzalez@gmail.com>
2024-06-04 22:55:31 +0200

    chore: Add layout and home page components

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="flex items-center justify-center py-8">
+        <h1 className="text-3xl font-bold text-black">Welcome to the NextJS App</h1>
+      </div>
+    </div>
+  );
+}
