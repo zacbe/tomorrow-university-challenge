6e1484c01a90dc5f539ed1ec686e932e037c1faa
@zacbe <zacbegonzalez@gmail.com>
2024-06-04 23:00:18 +0200

    feat: Add challenges and lessons pages

---
+++ app/(pages)/challenges/page.tsx
@@ -0,0 +1,9 @@
+import ChallengeTable from '@/app/components/ChallengeTable';
+
+export default function Challenges() {
+  return (
+    <div className="flex items-center justify-center py-8">
+      <ChallengeTable />
+    </div>
+  );
+}
---
+++ app/(pages)/lessons/page.tsx
@@ -0,0 +1,9 @@
+import LessonTable from '@/app/components/LessonTable';
+
+export default function Lessons() {
+  return (
+    <div className="flex items-center justify-center py-8">
+      <LessonTable />
+    </div>
+  );
+}
